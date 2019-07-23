fed93a215d21f3db78c571f1098df7a8915dfb1f
bambooka <sumkina.ol@yandex.ru>
2019-07-23 09:00:32 +0300

    feat: add router setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Route, Switch} from 'react-router-dom';
 
 import Home from './pages/HomePage'
 import About from './pages/AboutPage'
 import Contact from './pages/ContactPage'
-import Progucts from './pages/ProguctsPage'
-import SingleProgut from './pages/SingleProductPage'
+import Products from './pages/ProguctsPage'
+import SingleProduct from './pages/SingleProductPage'
+import Cart from './pages/CartPage'
 import Defaullt from './pages/Default'
 
 
 function App() {
   return (
-    <h1>hello from tech store</h1>
+
+  <Switch>
+    <Route path="/" exact component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/contact" component={Contact} />
+    <Route path="/products" exact component={Products} />
+    <Route path="/products/:id" component={SingleProduct} />
+    <Route path="/cart" component={Cart} />
+    <Route path="/default" component={Defaullt} />
+  </Switch>
   );
 }
 
