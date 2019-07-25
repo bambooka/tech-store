fa424bc5f96b702faa1f4dbe70c349734f62e980
bambooka <sumkina.ol@yandex.ru>
2019-07-25 17:36:23 +0300

    feat: custom default page
    Signed-off-by: bambooka <sumkina.ol@yandex.ru>

--- src/pages/DefaultPage.js
+++ src/pages/DefaultPage.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import Hero from '../component/Hero';
+import defaultBg from '../images/defaultBcg.jpeg';
+import {Link} from 'react-router-dom';
 
-export default function DefaultPage(){
-    return(
-        <h1>hello from default page</h1>
+export default function DefaultPage() {
+    return (
+        <>
+            <Hero img={defaultBg} max="true" title="404">
+                <h2 className="text-uppercase">page not found</h2>
+                <Link to="/" className="main-link">to home</Link>
+            </Hero>
+        </>
     )
 }
