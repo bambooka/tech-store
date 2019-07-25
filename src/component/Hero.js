696601fd118da41730e750a9d0c91580aecb67fa
bambooka <sumkina.ol@yandex.ru>
2019-07-25 17:18:05 +0300

    feat: custom main button on home page
    Signed-off-by: bambooka <sumkina.ol@yandex.ru>

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -21,7 +21,7 @@ const HeroWrapper = styled.div`
     justify-content: center;
     min-height: ${props => props.max ? '100vh' : '60vh'};
     color: var(--mainWhite);
-    background: linear-gradient(var(--primaryRGBA), var(--primaryRGBA)), url(${props => props.img} center/cover no-repeat);
+    background: linear-gradient(var(--primaryRGBA), var(--primaryRGBA)), url(${props => props.img}) center/cover no-repeat;
     
     .title {
         font-size: 3.5rem;
@@ -29,6 +29,26 @@ const HeroWrapper = styled.div`
         text-shadow: 4px 4px 2px rgba(0, 0, 0, 0.3);
         text-transform: uppercase;
         letter-spacing: var(--mainSpacing); 
+    }
+    
+    .main-link {
+        padding: 0.5rem;
+        display: inline-block;
+        background: var(--primaryColor);
+        outline: 4px solid var(--primaryColor);
+        outline-offset: 4px;
+        color: var(--mainWhite);
+        text-transform: uppercase;
+        letter-spacing: var(--mainSpacing);
+        transition: var(--mainTransition);
+        margin: 2rem;
+    }
+    
+    .main-link:hover {
+        background: var(--darkGrey);
+        outline-color: var(--darkGrey);
+        color: var(--primaryColor);
+        text-decoration: none;
     }
 `;
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,8 +5,8 @@ import {Link} from 'react-router-dom'
 export default function HomePage() {
     return (
         <>
-            <Hero title="awesome gadgets" max="true">
-                <Link to="/products">
+            <Hero title="awesome gadgets" max="true" img={'../images/mainBcg.jpeg'}>
+                <Link to="/products" className="main-link">
                     our products
                 </Link>
             </Hero>
