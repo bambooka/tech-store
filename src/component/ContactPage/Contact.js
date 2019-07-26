9a05324ed8ae4847c822f815a9504a70ff692454
bambooka <sumkina.ol@yandex.ru>
2019-07-26 10:44:29 +0300

    feat: custom contact page
    Signed-off-by: bambooka <sumkina.ol@yandex.ru>

---
+++ src/component/ContactPage/Contact.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Title from '../Title';
+
+export default function Contact(){
+    return(
+        <section className="py-5">
+            <div className="row">
+                <div className="col-10 col-md-6 mx-auto my-3">
+                    <Title title="contact us" style={{marginBottom: '2rem'}}/>
+                    <form className="form-group">
+                        <input type="text" className="form-control my-2" name="firstName" placeholder="John Smith"/>
+                        <input type="email" className="form-control my-2" name="email" placeholder="example@email.com"/>
+                        <input type="text" className="form-control my-2" name="subject" placeholder="Type your subject"/>
+                        <textarea name="message" className="form-control my-2" rows="5" placeholder="type your message"/>
+                    </form>
+                </div>
+            </div>
+        </section>
+    )
+}
--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Hero from '../component/Hero';
+import contactBg from '../images/contactBcg.jpeg';
+import Contact from '../component/ContactPage/Contact'
 
-export default function ContactPage(){
-    return(
-        <h1>hello from contact page</h1>
+
+export default function ContactPage() {
+    return (
+        <>
+            <Hero img={contactBg}/>
+            <Contact/>
+        </> 
     )
 }
