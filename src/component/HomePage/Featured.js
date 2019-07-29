dc9b3bcac6ec1598bb84d9e19f47aba8d3c748ac
bambooka <sumkina.ol@yandex.ru>
2019-07-29 08:02:01 +0300

    feat: add Product component with a base logic
    Signed-off-by: bambooka <sumkina.ol@yandex.ru>

--- src/component/HomePage/Featured.js
+++ src/component/HomePage/Featured.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import Product from '../Product'
+import Title from '../Title'
+import {Link} from 'react-router-dom'
+import {ProductConsumer} from "../../context";
 
 export default function Featured () {
     return(
-        <h1> featurede</h1>
+        <section className="py-5">
+            <div className="container">
+                <Title title="featured product" center="true"/>
+                <div className="row">
+                    <ProductConsumer>
+                        {value => {
+                            const {featuredProducts} = value;
+                            return featuredProducts.map(product => (
+                                <Product key={product.id} product={product}/>
+                            ))
+                        }}
+                    </ProductConsumer>
+                </div>
+            </div>
+        </section>
     )
 }
