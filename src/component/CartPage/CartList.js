7b57c60a185ea5336ffb521349465820a5a99c13
bambooka <sumkina.ol@yandex.ru>
2019-08-04 15:09:12 +0300

    feat: add cart list structure
    Signed-off-by: bambooka <sumkina.ol@yandex.ru>

--- src/component/CartPage/CartList.js
+++ src/component/CartPage/CartList.js
@@ -1,9 +1,44 @@
 import React from 'react';
-import CartItem from './CartItem'
+import CartItem from './CartItem';
+import  { ProductConsumer} from "../../context";
 
 export default function CartList () {
     return(
-        <div>cart list</div>
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col">
+                    <ProductConsumer>
+                        {
+                            value => {
+                                const {cart, increment, decrement, removeItem} = value;
+                                if(cart.length === 0 ){
+                                    return(
+                                    <h1 className="text-title text-center mb-4">
+                                        your cart is currently empty
+                                    </h1>
+                                );
+                                }
+                                return (
+                                    <>
+                                        {cart.map(item => {
+                                            return <CartItem
+                                                key={item.id}
+                                                cartItem={item}
+                                                increment={increment}
+                                                decrement={decrement}
+                                                removeItem={removeItem}
+                                            />
+                                        })}
+                                    </>
+                                )
+                            }
+                        }
+
+                    </ProductConsumer>
+                </div>
+            </div>
+
+        </div>
     )
 
 }
