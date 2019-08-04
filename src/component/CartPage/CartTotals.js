1a6b427a5da473a35ff699978edcc6ea5c5a28d5
bambooka <sumkina.ol@yandex.ru>
2019-08-04 14:51:03 +0300

    feat: add total info about cart & clear button
    Signed-off-by: bambooka <sumkina.ol@yandex.ru>

--- src/component/CartPage/CartTotals.js
+++ src/component/CartPage/CartTotals.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import {ProductConsumer} from "../../context";
 
-export default function CartTotals () {
-    return(
-        <div>cart totals</div>
+export default function CartTotals() {
+    return (
+        <div className="container">
+            <div className="row">
+                <ProductConsumer>
+                    {value => {
+                        const {clearCart, cartSubTotal, cartTax, cartTotal} = value;
+                        return <div className="col text-title text-center my-4">
+                            <button className="btn btn-outline-danger text-capitalize mb-4"
+                                    onClick={clearCart}
+                            >
+                                clear cart
+                            </button>
+                            <h3>SubTotal : ${cartSubTotal}</h3>
+                            <h3>cart Tax : ${cartTax}</h3>
+                            <h3>Total : ${cartTotal}</h3>
+
+                        </div>
+                    }}
+                </ProductConsumer>
+            </div>
+        </div>
     )
 
 }
