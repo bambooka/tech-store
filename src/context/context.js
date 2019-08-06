9753644a809334d265c53ce4c64352b2cd2a30ae
bambooka <sumkina.ol@yandex.ru>
2019-08-06 08:21:19 +0300

    feat: add clear cart method
    Signed-off-by: bambooka <sumkina.ol@yandex.ru>

--- src/context/context.js
+++ src/context/context.js
@@ -187,7 +187,24 @@ class ProductProvider extends React.Component {
     };
 
     decrement = (id) => {
-        console.log(id)
+        let tempCart = [...this.state.cart];
+        const cartItem = tempCart.find(item => item.id === id);
+
+        cartItem.count = cartItem.count - 1;
+        if(cartItem.count === 0){
+            this.removeItem(id);
+        } else {
+            cartItem.total = cartItem.count * cartItem.price;
+            cartItem.total = parseFloat(cartItem.total.toFixed(2));
+            this.setState(() => {
+                return {
+                    cart: [...tempCart]
+                }
+            }, ()=> {
+                this.addTotals();
+                this.syncStorage();
+            })
+        }
     };
 
     removeItem = (id) => {
@@ -204,7 +221,14 @@ class ProductProvider extends React.Component {
     };
 
     clearCart = () => {
-        console.log('cart is cleaned')
+        this.setState(() => {
+            return {
+                cart: []
+            }
+        }, () => {
+            this.addTotals();
+            this.syncStorage();
+        })
     };
 
     render() {
